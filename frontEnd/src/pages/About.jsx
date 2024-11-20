f9d8c69023dba49873cdbda06ed10d7dfe1cd204
mohamedhamdy9966 <mh7008002@gmail.com>
2024-11-20 03:58:49 +0200

    Add .env to .gitignore

--- frontEnd/src/pages/About.jsx
+++ frontEnd/src/pages/About.jsx
@@ -1,10 +1,46 @@
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import NewsLetterBox from "../components/NewsLetterBox";
 
 const About = () => {
   return (
     <div>
-      
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"About"} text2={"Us"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          src={assets.about_img}
+          alt="AboutUs"
+          className="w-full md:max-w-[450px]"
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p></p>
+          <p></p>
+          <b className="text-gray-800"></b>
+          <p></p>
+        </div>
+      </div>
+      <div>
+        <Title text1={"Why"} text2={"Choose Us"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>mnnvx</b>
+          <p className="text-gray-600">fdjkfjifeyuweiwuewec</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>mnnvx</b>
+          <p className="text-gray-600">fdjkfjifeyuweiwuewec</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>mnnvx</b>
+          <p className="text-gray-600">fdjkfjifeyuweiwuewec</p>
+        </div>
+      </div>
+      <NewsLetterBox />
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- frontEnd/src/pages/Login.jsx
+++ frontEnd/src/pages/Login.jsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 
 const Login = () => {
   const [currentState, setCurrentState] = useState("SignUp");
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
@@ -58,6 +64,9 @@ const Login = () => {
           </p>
         )}
       </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === "Login" ? "Sign In" : "SignUP"}
+      </button>
     </form>
   );
 };
